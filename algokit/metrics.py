fdf71a07977d215fce7e62bfc18b360bb65cf404
Prasanna Muppidwar <mprasanna5557@gmail.com>
2024-10-13 21:19:38 +0530

    Evaluation Metrics Updated!

--- algokit/metrics.py
+++ algokit/metrics.py
@@ -1,14 +1,12 @@
 import numpy as np
 
 def mean_squared_error(y_true, y_pred):
-    # Convert input to numpy arrays for easier manipulation
+    """
+    Calculate mean squared error to evaluate the model performance.
+    """
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
-    
-    # Compute the squared differences
     squared_diff = (y_true - y_pred) ** 2
-    
-    # Return the mean of these squared differences
     return np.mean(squared_diff)
 
 def r2_score(y_true, y_pred):
