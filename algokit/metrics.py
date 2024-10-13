30075e908b343cf71f9374e9a45edcebfdb14187
Prasanna Muppidwar <mprasanna5557@gmail.com>
2024-10-13 21:13:14 +0530

    evakuation metrics updated

--- algokit/metrics.py
+++ algokit/metrics.py
@@ -10,3 +10,11 @@ def mean_squared_error(y_true, y_pred):
     
     # Return the mean of these squared differences
     return np.mean(squared_diff)
+
+def r2_score(y_true, y_pred):
+    """
+    Calculate the R-squared score to evaluate the model performance.
+    """
+    corr_matrix = np.corrcoef(y_true, y_pred)
+    corr = corr_matrix[0, 1]
+    return corr ** 2
